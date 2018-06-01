0f643a9bcafef8df04a0e98775b5b2d8c75d0cf0
eileengonzales <gonza338@gmail.com>
2018-06-01 17:12:47 -0400

    Made modifications to Run for me and 1256.

--- 1256_mcmc.py
+++ 1256_mcmc.py
@@ -4,6 +4,8 @@ import mcmc_fit.mcmc_fit as mc
 import numpy as np
 import time
 start_time = time.time()
+#from synth_fit.Calc_chisquare_modifiedEG import *
+from synth_fit.calc_chisq import *
 
 
 # Read in SED of 1256-0224
@@ -33,12 +35,12 @@ wavelength, flux, err = np.loadtxt('SED1256_nir_Fire.txt', delimiter="\t", unpac
 model_atmosphere_db = '/Users/eileengonzales/Dropbox/BDNYC/BDNYCdb_copy/model_atmospheres.db'
 # Get the BtSettl grid
 mg = mc.make_model_db('bt_settl_2013', model_atmosphere_db, grid_data='spec',
-                      param_lims=[('teff', 2000, 2500, 50), ('logg', 3.5, 5.5, 0.5)], fill_holes=False, bands=[],
-                      rebin_models=wavelength, use_pandas=False)
+                      param_lims=[('teff', 1200, 3000, 50), ('logg', 3.5, 5.5, 0.5)], fill_holes=False, bands=[],
+                      rebin_models=w, use_pandas=False)
 
 # Get Marley & Saumon. f_sed is only 2 for temps above 1600 K
 mg2 = mc.make_model_db('marley_saumon', model_atmosphere_db, grid_data='spec',
-                       param_lims=[('teff', 2000, 2400, 50), ('logg', 3.5, 5.5, 0.5), ('f_sed', 0, 2, 1)],
+                       param_lims=[('teff', 1600, 2400, 50), ('logg', 3.5, 5.5, 0.5), ('f_sed', 0, 2, 1)],
                        fill_holes=False, bands=[], rebin_models=w, use_pandas=False)
 
 # Adding units to the arrays for either way grid is generated
@@ -59,17 +61,99 @@ err = err * q.erg/q.AA/q.cm**2/q.s
 # bdsamp = mc.fit_spectrum([w, f, e], mg, 'bt_settl_2013', '1256-0224', 1, 5, mask=[], db='', extents=None,
 #                          object_name='J1256', log=False, plot=True, prnt=True, generate=True, outfile=None)
 
-
+# --------------- Probably won't run the MCMC and will just do a Chi-sqaured analysis for the report. ----------------
 # Full Run on J1256 trying 3 walkers and 100 steps to get an idea of how long the run takes.
-# bdsamp = mc.fit_spectrum([w, f, e], mg, 'bt_settl_2013', '1256-0224', 2, 5, mask=[], db='', extents=None,
-#                          run_name='J1256_test', log=False, plot=True, prnt=True, generate=True, outfile=None)
+bdsamp = mc.fit_spectrum([w, f, e], mg, 'bt_settl_2013', '1256-0224chi', 2, 5, mask=[], db='', range=None,
+                         run_name='J1256_testchi', log=False, plot=True, prnt=True, generate=True, outfile=None)
 
 bdsamp_nir = mc.fit_spectrum([wavelength, flux, err], mg, 'bt_settl_2013', '1256-0224', 2, 5, mask=[], db='', range=None,
                          run_name='J1256_nironly', log=False, plot=True, prnt=True, generate=True, outfile=None)
 
-# bdsamp2 = mc.fit_spectrum([w, f, e], mg2, 'marley_saumon', '1256-0224', 5, 100, mask=[], db='', range=None,
-#                           run_name='J1256_marley', log=False, plot=True, prnt=True, generate=True, outfile=None)
+bdsamp2 = mc.fit_spectrum([w, f, e], mg2, 'marley_saumon', '1256-0224test', 5, 100, mask=[], db='', range=None,
+                          run_name='J1256_test', log=False, plot=True, prnt=True, generate=True, outfile=None)
 
 # determine time it takes code to run 2, walkers 5 steps 11.07 minutes. 3 walkers, 100 steps -7.8 hours
 # nir only 2,5 = 7.8 minutes
 print "time elapsed: {:.2f}s".format(time.time() - start_time)
+
+# Run the Chi-squared individually
+test_all(w,f,e,mg2,['teff','logg','f_sed','k_zz'],smooth=False, resolution=None, shortname='J1256-0224_tf')
+#Output:  (array([  2.40000000e+03,   4.00000000e+00,   2.00000000e+00, 0.00000000e+00]), 0.17684231505573014)
+
+test_all(w,f,e,mg,['teff','logg'],smooth=False, resolution=None, shortname='J1256-0224_tfbt')
+# Output: (array([ 1650.,     4.]), 0.014132433189028338)
+
+
+# -------- Function needed to plot the model ----------------
+
+def rebin_spec(spec, wavnew, waveunits='um'):
+    from pysynphot import spectrum, observation
+    # Gives same error answer: Err = np.array([np.sqrt(sum(spec[2].value[idx_include(wavnew,[((wavnew[0] if n==0 else
+    #  wavnew[n-1]+wavnew[n])/2,wavnew[-1] if n==len(wavnew) else (wavnew[n]+wavnew[n+1])/2)])]**2)) for n in range(
+    # len(wavnew)-1)])*spec[2].unit if spec[2] is not '' else ''
+    if len(spec) == 2:
+        spec += ['']
+    try:
+        Flx, Err, filt = spectrum.ArraySourceSpectrum(wave=spec[0].value,
+                                                      flux=spec[1].value), spectrum.ArraySourceSpectrum(
+            wave=spec[0].value, flux=spec[2].value) if spec[2] else '', spectrum.ArraySpectralElement(spec[0].value,
+                                                                                                      np.ones(
+                                                                                                          len(spec[0])),
+                                                                                                      waveunits=waveunits)
+    except:
+        spec, wavnew = [i * q.Unit('') for i in spec], wavnew * q.Unit('')
+        Flx, Err, filt = spectrum.ArraySourceSpectrum(wave=spec[0].value,
+                                                      flux=spec[1].value), spectrum.ArraySourceSpectrum(
+            wave=spec[0].value, flux=spec[2].value) if spec[2] else '', spectrum.ArraySpectralElement(spec[0].value,
+                                                                                                      np.ones(
+                                                                                                          len(spec[0])),
+                                                                                                      waveunits=waveunits)
+    return [wavnew, observation.Observation(Flx, filt, binset=wavnew.value, force='taper').binflux * spec[1].unit,
+            observation.Observation(Err, filt, binset=wavnew.value, force='taper').binflux * spec[2].unit if spec[
+                2] else np.ones(len(wavnew)) * spec[1].unit]
+
+# -------------------------------------
+
+
+# Pull out the models in order to plot them up.
+ma_db = astrodb.Database(model_atmosphere_db)
+ms_nocloud = ma_db.query("select wavelength, flux from marley_saumon where logg=4.0 and teff=2400 and f_sed=2 and k_zz=0", fmt='dict')
+bt_cloud = ma_db.query("select wavelength, flux from bt_settl_2013 where logg=4.0 and teff=1650", fmt='dict')
+
+
+# pull out the wavelenght and flux
+flux_nc=ms_nocloud[0]['flux']
+wave_nc=ms_nocloud[0]['wavelength']
+flux_cl=bt_cloud[0]['flux']
+wave_cl=bt_cloud[0]['wavelength']
+
+# Normalize to max flux and smooth to same resolution
+norm_flnc=flux_nc/max(flux_nc)
+norm_flcl=flux_cl/max(flux_cl)
+
+# create an uncertainty array for the model
+unc = np.ones(len(norm_flnc))
+unc2 = np.ones(len(norm_flcl))
+
+spec = [wave_nc,norm_flnc,unc]   # group it together
+speck = rebin_spec(spec,w)  # rebin to the same wavelength as 1256.  Is this effectively smoothing????
+spec2 = [wave_cl,norm_flcl,unc2]   # group it together
+speck2 = rebin_spec(spec2,w)
+
+
+wl_bin_nc = speck[0].value
+fl_bin_nc = speck[1].value
+wl_bin_cl = speck2[0].value
+fl_bin_cl = speck2[1].value
+
+# Normalize SED of 1256 to peak flux
+norm_1256 = f/max(f)
+
+plt.plot(wl_bin_nc,fl_bin_nc)
+plt.plot(wl_bin_cl,fl_bin_cl)
+plt.plot(w,norm_1256)
+
+# TODO: Add legend, axis labels and all the good stuff!
+
+
+
--- synth_fit/Calc_chisquare_modifiedEG.py
+++ synth_fit/Calc_chisquare_modifiedEG.py
@@ -1,6 +1,7 @@
 # Calculate Chi-Squared for all models in a grid to determine the starting
 # point for the emcee walkers
 # Stephanie Douglas
+# Modifed by Eileen Gonzales to just use for a chi-squared calculation.
 ################################################################################
 
 import logging
@@ -8,13 +9,15 @@ import logging
 import numpy as np
 from astropy import units as u
 import pickle
-from synth_fit.smooth import *
+# from synth_fit.smooth import *  # I will not be smoothing in the code.
 import matplotlib.colors as colors
 
 from matplotlib.colors import ListedColormap
 from matplotlib.colors import LinearSegmentedColormap
 from pylab import *
+
 cmap = cm.get_cmap('RdPu')
+
 
 def discrete_cmap(N, base_cmap=None):
     """Create an N-bin discrete colormap from the specified input map"""
@@ -28,26 +31,30 @@ def discrete_cmap(N, base_cmap=None):
     cmap_name = base.name + str(N)
     return base.from_list(cmap_name, color_list, N)
 
+
 cmap = plt.get_cmap('RdPu')
-sub_cmap = LinearSegmentedColormap.from_list('trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=0.2, b=1),cmap(np.linspace(0.2, 1, 6)))
+sub_cmap = LinearSegmentedColormap.from_list('trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=0.2, b=1),
+                                             cmap(np.linspace(0.2, 1, 6)))
 new_cmap = discrete_cmap(6, base_cmap=sub_cmap)
 
+
 def calc_chisq(data_flux, data_unc, model_flux):
-    a = (data_flux-model_flux)**2
-    b = (data_unc**2)
-    cs=[]
+    a = (data_flux - model_flux) ** 2
+    b = (data_unc ** 2)
+    cs = []
     for i in range(len(a)):
-    	if float(b[i].value)==0.0:
-    		bb==0.000000000000000001
-    	else: bb=float(b[i].value)
-    	c = float(a[i].value)/float(bb)
-    	cs.append(c)
+        if float(b[i].value) == 0.0:
+            bb == 0.000000000000000001
+        else:
+            bb = float(b[i].value)
+        c = float(a[i].value) / float(bb)
+        cs.append(c)
     d = np.sum(cs)
     return c
 
 
 def test_all(data_wave, data_flux, data_unc, model_dict, params,
-    smooth=False, resolution=None, shortname=''):
+             smooth=False, resolution=None, shortname=''):
     """
     Calculates chi-squared for all models in a grid to determine
     the starting point for the emcee walkers (or just to find the
@@ -85,92 +92,100 @@ def test_all(data_wave, data_flux, data_unc, model_dict, params,
 
     """
 
-    #logging.debug(data_wave.unit.to_string('fits'))
-    #logging.debug(data_unc.unit.to_string('fits'))
-    #logging.debug(model_dict['wavelength'].unit.to_string('fits'))
-    if data_wave.unit!=model_dict['wavelength'].unit:
-         logging.debug('changing units')
-         data_wave = data_wave.to(model_dict['wavelength'].unit)
-    #logging.debug(data_wave.unit.to_string('fits'))
+    # logging.debug(data_wave.unit.to_string('fits'))
+    # logging.debug(data_unc.unit.to_string('fits'))
+    # logging.debug(model_dict['wavelength'].unit.to_string('fits'))
+    if data_wave.unit != model_dict['wavelength'].unit:
+        logging.debug('changing units')
+        data_wave = data_wave.to(model_dict['wavelength'].unit)
+    # logging.debug(data_wave.unit.to_string('fits'))
 
-    if ((len(model_dict['wavelength'])==len(data_wave)) and
-        (np.sum(model_dict['wavelength']-data_wave)<(model_dict['wavelength'].unit*1e-12))):
+    if ((len(model_dict['wavelength']) == len(data_wave)) and
+            (np.sum(model_dict['wavelength'] - data_wave) < (model_dict['wavelength'].unit * 1e-12))):
         interp = False
         logging.info('calc_chisq.test_all: NO INTERPOLATION')
     else:
         interp = True
         logging.info('calc_chisq.test_all: INTERPOLATION NEEDED')
 
-
     ndim = len(params)
 
     num_models = len(model_dict['flux'])
 
-    chisq = np.ones(num_models)*(99e15)
+    chisq = np.ones(num_models) * (99e15)
 
     save_chisq = []
 
     for i in range(num_models):
-#        logging.debug('%d %d %f %f',i, num_models, model_dict['logg'][i],
-#            model_dict['teff'][i])
+        #        logging.debug('%d %d %f %f',i, num_models, model_dict['logg'][i],
+        #            model_dict['teff'][i])
         if smooth:
-            mod_flux = falt2(model_dict['wavelength'],model_dict['flux'][i],resolution)
+            mod_flux = falt2(model_dict['wavelength'], model_dict['flux'][i], resolution)
         else:
             mod_flux = model_dict['flux'][i]
-            #logging.debug('shape flux {} mf {}'.format(np.shape(model_dict['flux']), np.shape(mod_flux)))
+            # logging.debug('shape flux {} mf {}'.format(np.shape(model_dict['flux']), np.shape(mod_flux)))
 
-        #logging.debug('lengths dw {} modw {} modf {}'.format(
+        # logging.debug('lengths dw {} modw {} modf {}'.format(
         #    len(data_wave),len(model_dict['wavelength']),len(mod_flux)))
         if interp:
             mod_flux = np.interp(data_wave, model_dict['wavelength'], mod_flux)
-#         mod_flux=mod_flux*u.erg/u.AA/u.cm**2/u.s
-#        logging.debug(str(mod_flux[100:110]))
-#        logging.debug('stdev %f', np.std(mod_flux))
-#
-#         mult1 = data_flux*mod_flux
-#         bad = np.isnan(mult1)
-#         mult = np.sum(mult1[~bad])
-#         sq1 = mod_flux**2
-#         square = np.sum(sq1[~bad])
-#         ck = mult/square
-#         mod_flux = mod_flux*ck
-        mult1 = data_flux*mod_flux/(data_unc**2)
+        # mod_flux=mod_flux*u.erg/u.AA/u.cm**2/u.s
+        #        logging.debug(str(mod_flux[100:110]))
+        #        logging.debug('stdev %f', np.std(mod_flux))
+        #
+        #         mult1 = data_flux*mod_flux
+        #         bad = np.isnan(mult1)
+        #         mult = np.sum(mult1[~bad])
+        #         sq1 = mod_flux**2
+        #         square = np.sum(sq1[~bad])
+        #         ck = mult/square
+        #         mod_flux = mod_flux*ck
+        #  --------- This is where the normalization by a constant happens -------------
+        mult1 = data_flux * mod_flux / (data_unc ** 2)
         bad = np.isnan(mult1)
         mult = np.sum(mult1[~bad])
-        sq1 = mod_flux*mod_flux/(data_unc**2)
+        sq1 = mod_flux * mod_flux / (data_unc ** 2)
         mult2 = float(sum(sq1[~bad]))
-        ck = mult/mult2
-        mod_flux=mod_flux*ck
+        ck = mult / mult2
+        mod_flux = mod_flux * ck
 
         chisq[i] = calc_chisq(data_flux, data_unc, mod_flux)
         foo = plt.scatter(model_dict['teff'][i], chisq[i], c=model_dict['logg'][i], cmap=new_cmap, edgecolor='None',
                           vmin=2.75, vmax=5.75)
         params_list = []
         for p in params:
-         params_list.append(model_dict[p][i])
-        save_chisq.append([params_list,chisq[i]])
+            params_list.append(model_dict[p][i])
+        save_chisq.append([params_list, chisq[i]])
 
     min_loc = np.argmin(chisq)
-#    logging.debug('min_loc %d', min_loc)
+    #    logging.debug('min_loc %d', min_loc)
     best_params = np.array([])
     for p in params:
         best_params = np.append(best_params, model_dict[p][min_loc])
 
-    plt.xlabel('$T_{eff}$',fontsize='x-large')
+    plt.xlabel('$T_{eff}$', fontsize='x-large')
     plt.ylabel('Goodness of Fit', fontsize='x-large')
-    plt.annotate('{}, {}'.format(shortname, best_params), xy=(0.5, 0.95), xycoords='axes fraction')
+    plt.annotate('{}'.format(shortname), xy=(0.75, 0.95), xycoords='axes fraction')
+    # Double {{}} to get format to work, '.2f' means 2 decimals for a float
+    plt.annotate('$T_\mathrm{{eff}}$={}'.format(best_params[0], '.1f'), xy=(0.75, 0.9), xycoords='axes fraction')
+    plt.annotate('log $g$={}'.format(best_params[1], '.2f'), xy=(0.75, 0.85), xycoords='axes fraction')
+    plt.annotate('$f_\mathrm{{sed}}$={}'.format(best_params[2], '.2f'), xy=(0.75, 0.8), xycoords='axes fraction')
+    plt.annotate('$K_\mathrm{{zz}}$={}'.format(best_params[3], '.2f'), xy=(0.75, 0.75), xycoords='axes fraction')
     fig = plt.gcf()
     labels = np.arange(3.0, 6.0, 0.5)
     fig.subplots_adjust(right=0.8)
     cbar_ax = fig.add_axes([0.85, 0.15, 0.03, 0.7])
     cbar = fig.colorbar(foo, cax=cbar_ax, ticks=labels)
     cbar.set_label(label='$log(g)$', size=18)
-    #plt.savefig('/Users/paigegiorla/Desktop/Teff_goodnesses_{}'.format(shortname)+'.pdf')
-    plt.savefig('/Users/eileengonzales/Dropbox/BDNYC/BDNYC_Research/Python/Modules/synth_fit/Run_outputs/Figures/Teff_goodnesses_{}'.format(shortname) + '.pdf')
+    # plt.savefig('/Users/paigegiorla/Desktop/Teff_goodnesses_{}'.format(shortname)+'.pdf')
+    plt.savefig(
+        '/Users/eileengonzales/Dropbox/BDNYC/BDNYC_Research/Python/Modules/synth_fit/Run_outputs/Figures/Teff_goodnesses_{}'.format(
+            shortname) + '.pdf')
     plt.clf()
 
-
-    fb = open('/Users/eileengonzales/Dropbox/BDNYC/BDNYC_Research/Python/Modules/synth_fit/Run_outputs/chisquares_{}'.format(shortname)+'.pkl','wb')
+    fb = open(
+        '/Users/eileengonzales/Dropbox/BDNYC/BDNYC_Research/Python/Modules/synth_fit/Run_outputs/chisquares_{}'.format(
+            shortname) + '.pkl', 'wb')
     pickle.dump(save_chisq, fb)
     fb.close()
     return best_params, min(chisq)
